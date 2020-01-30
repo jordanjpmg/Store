59cd82cacf87f4161fe42f19fee9df168cd258b0
jordanjpmg <jordanjpmg@gmail.com>
2020-01-30 15:21:05 -0300

    Respostas desafio Básico,Intermediario e Avançado

--- js/header-store.js
+++ js/header-store.js
@@ -1,3 +1,10 @@
-const $search = document.querySelector("[type = search]");
+const $searchIcon = document.querySelector(".icon");
+const $search = document.querySelector("[type=search]");
 
-$search.addEventListener("click", () => $search.classList.toggle("-active"));
+$searchIcon.addEventListener("click", () =>
+  $search.classList.toggle("-active")
+);
+
+$searchIcon.addEventListener("click", () =>
+  $searchIcon.classList.toggle("-active")
+);
