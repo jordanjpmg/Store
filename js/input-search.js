c865484e2ea2fdca924183a81612f353239ba772
jordanjpmg <jordanjpmg@gmail.com>
2020-02-10 17:31:40 -0300

    Finalizando Menu Hamburguer

--- js/input-search.js
+++ js/input-search.js
@@ -1,4 +1,4 @@
-const $searchIcon = document.querySelector(".icon-search");
+/*const $searchIcon = document.querySelector(".icon-search");
 const $search = document.querySelector(".input-search");
 
 $searchIcon.addEventListener("click", () =>
@@ -7,4 +7,24 @@ $searchIcon.addEventListener("click", () =>
 
 $searchIcon.addEventListener("click", () =>
   $searchIcon.classList.toggle("-active")
-);
+);*/
+
+
+
+const $inputSearch = document.querySelector(".input-search");
+const $search = $inputSearch.querySelector(".-search")
+const $close = $inputSearch.querySelector(".-close")
+
+$search.addEventListener("click", event => {
+  event.preventDefault();
+  $inputSearch.classList.toggle("-active")
+  $search.classList.remove("-active")
+  $close.classList.add("-active")
+})
+
+$close.addEventListener("click", event => {
+  event.preventDefault();
+  $inputSearch.classList.toggle("-active")
+  $close.classList.remove("-active")
+  $search.classList.add("-active")
+})
